de1626e134153cef189a74b0ccf04fc44b21d462
aravindaet32 <aravindaet06@gmail.com>
2025-05-13 12:09:29 +0530

    Create multiplication while.c

---
+++ multiplication while.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int number, i = 1;
+
+    
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    
+    printf("Multiplication table of %d:\n", number);
+    while (i <= 10) {
+        printf("%d x %d = %d\n", number, i, number * i);
+        i++;
+    }
+
+    return 0;
+}
