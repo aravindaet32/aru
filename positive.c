bf912935df08148db26b30d16ebfadfc0e302168
aravindaet32 <aravindaet06@gmail.com>
2025-05-13 10:47:33 +0530

    Create positive.c

---
+++ positive.c
@@ -0,0 +1,16 @@
+  printf("the number is negitive.\n");
+        } else{
+            printf("the number is zero.\n");
+        }
+        return 0;
+    }#include<stdio.h>
+int main()
+{
+    float number;
+     
+    printf("enter a number:");
+    scanf("%f",&number);
+    if(number>0){
+        printf("the number is positive.\n");
+    } else if (number <0) {
+    
