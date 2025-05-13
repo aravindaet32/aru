ab93a9b326ba46cd72768d9ba4fb585c5964c786
aravindaet32 <aravindaet06@gmail.com>
2025-05-14 00:32:45 +0530

    Create name,age, location.C

---
+++ name,age, location.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int age;
+    char name[100];
+    char location[100];
+    printf("enter the name:");
+    scanf("%s",&name);
+    printf("enter your age:");
+    scanf("%d",&age);
+    printf("enter your location:");
+    scanf("%s",&location);
+    
+}
