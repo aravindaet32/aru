9fdde94960d533d931df07e5a367aeb16f294dc6
aravindaet32 <aravindaet06@gmail.com>
2025-05-13 15:47:10 +0530

    Create person deatails.c

---
+++ person deatails.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int shridevi()
+{
+     printf("aravnda\n");
+     printf("dateof birth\n");
+     printf("age\n");
+     printf("mobilenumber\n");
+} 
+int main()
+{
+    shridevi();
+    return 0;
+}
+     
