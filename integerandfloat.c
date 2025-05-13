53c649fa9b731b7e396ffbe6e6a39c753510a89f
aravindaet32 <aravindaet06@gmail.com>
2025-05-14 00:38:48 +0530

    Create integerandfloat.c

---
+++ integerandfloat.c
@@ -0,0 +1,11 @@
+
+#include<stdio.h>
+int main()
+{
+    int A;
+    float B;
+    printf("enter an integer_value:\n");
+    scanf("%d",&A);
+    printf("enter a float_value:\n");
+    scanf("%f",&B);
+}
