bb27057d4e0d2a12badaaf5ad795221aaaecf5f3
aravindaet32 <aravindaet06@gmail.com>
2025-05-14 07:01:27 +0530

    Create name_age_locetion.c

---
+++ name_age_locetion.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    char name[] = "Aravinda";
+    int age = 19;
+    char location[] = "Harapanahalli, Vijayanagar";
+    printf("Name: %s\n", name);
+    printf("Age: %d\n", age);
+    printf("Location: %s\n", location);
+    return 0;
+}
