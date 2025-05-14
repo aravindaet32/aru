97172c231c00c191ca1a175bb48f268fd6683d41
aravindaet32 <aravindaet06@gmail.com>
2025-05-14 07:07:14 +0530

    Create old_age.c

---
+++ old_age.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    char name[50];
+    int age;
+    printf("Enter your name: ");
+    fgets(name, sizeof(name), stdin);
+    printf("Enter your age: ");
+    scanf("%d", &age);
+    printf("Hello %s, you are %d years old.\n", name, age);
+    return 0;
+}
