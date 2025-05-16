ee0d227842b0a3cc58152b5a921cacddd59c138d
aravindaet32 <aravindaet06@gmail.com>
2025-05-16 23:30:37 +0530

    Create employee_detals.c

---
+++ employee_detals.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+struct Address{
+    char city[20];
+    int pin;
+};
+struct Employee{
+    char name[50];
+    int id;
+    struct Address addr;
+};
+int main(){
+    struct Employee emp;
+    printf("Enter name:");
+    scanf("%s",emp.name);
+    printf("Enter city:");
+    scanf("%s",emp.addr.city);
+    printf("Enter pin:");
+    scanf("%d",&emp.addr.pin);
+    printf("\nEMPLOYEE DETAILS\n");
+    printf("Name:%s\n City:%s\n Pin:%d\n",emp.name,emp.addr.city,emp.addr.pin);
+}
